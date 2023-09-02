3d457b90a30257bce1c7c5259e47e4d2e76addfc
Alexander Wilde <alexander.wilde87@gmail.com>
2023-09-02 22:41:23 +0100

    Reuse finding images code

--- src/server/main.ts
+++ src/server/main.ts
@@ -5,6 +5,16 @@ import ViteExpress from 'vite-express'
 import config from './config'
 import './cron'
 
+const listImages = async () => {
+  await fs.mkdir(config.imageDir, { recursive: true })
+  const files = await fs.readdir(config.imageDir, {
+    withFileTypes: true,
+  })
+  return files
+    .filter(file => file.isFile() && config.imageExts.includes(path.extname(file.name)))
+    .map(file => file.name)
+}
+
 const app = express()
 
 const parseAuthorization = (value: string | undefined) => {
@@ -29,34 +39,33 @@ app.use((req, res, next) => {
 })
 
 app.get('/images', async (req, res) => {
-  await fs.mkdir(config.imageDir, { recursive: true })
-  const files = await fs.readdir(config.imageDir, {
-    withFileTypes: true,
-  })
-  const images = await Promise.all(files
-    .filter(file => file.isFile() && config.imageExts.includes(path.extname(file.name)))
-    .map(async file => {
-      const stat = await fs.stat(path.join(config.imageDir, file.name))
+  const images = await listImages()
+  const data = await Promise.all(images
+    .map(async image => {
+      const stat = await fs.stat(path.join(config.imageDir, image))
       return {
-        file: file.name,
+        file: image,
         uploaded: stat.birthtime.toISOString(),
       }
     }))
-  images.sort((a, b) => -a.uploaded.localeCompare(b.uploaded))
-  return res.status(200).json(images).end()
+  data.sort((a, b) => -a.uploaded.localeCompare(b.uploaded))
+  return res.status(200).json(data).end()
 })
 
 app.get('/images/:image', async (req, res) => {
-  return res.download(path.join(config.imageDir, req.params.image))
+  const images = await listImages()
+  const image = images.find(file => file == req.params.image)
+  if (image == undefined) return res.status(404).end()
+
+  return res.download(path.join(config.imageDir, image))
 })
 
 app.get('/random_image', async (req, res) => {
-  await fs.mkdir(config.imageDir, { recursive: true })
-  const files = await fs.readdir(config.imageDir, {
-    withFileTypes: true,
-  })
-  const index = Math.floor(Math.random() * files.length)
-  return res.download(path.join(config.imageDir, files[index].name))
+  const images = await listImages()
+  if (images.length == 0) return res.status(404).end()
+
+  const index = Math.floor(Math.random() * images.length)
+  return res.download(path.join(config.imageDir, images[index]))
 })
 
 ViteExpress.listen(app, config.port, () =>
